ca2047240c15174d2be5b94175b4d70ab3675a39
Frederick Roegiers <frederick@rogerthat.be>
2023-02-13 20:08:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# We Are PGM APP
+
+Dit is de kick-off applicatie uit de eerste les.
+
+Deze applicatie heeft als functie om een lijst van PGM studenten te tonen of om jezelf toe te voegen aan de lijst.
+
+## Hoe werkt het?
+
+Voer het volgende commando uit in de terminal:
+
+```bash
+$ npm install
+```
+
+Dit commando installeert alle benodigde packages.
+
+Vervolgens voer je het volgende commando uit:
+
+```bash
+$ npm run dev
+```
---
+++ src/app.js
@@ -0,0 +1,45 @@
+import fetch from "node-fetch";
+
+// declare the API_URL to post myself to
+const API_URL = "https://pgm.cmsdevelopment.be/api/student";
+
+// change the student object to your own data
+const student = {
+  firstname: "John",
+  lastname: "Doe",
+  nickname: "Johny",
+  classname: "1PGM-a",
+  email: "john_doe3@hotmail.com",
+  age: 32,
+  avatar: "https://i.pravatar.cc/300",
+  hobbies: ["Gamen", "Programmeren"],
+  motto: "Code like a boss",
+  about: "Ik ben een student die graag codeert.",
+};
+
+/**
+ * Fetch the students from the API
+ * Log the students to the console
+ */
+const getStudents = async () => {
+  const response = await fetch(API_URL);
+  const students = await response.json();
+  console.log(students);
+};
+// uncomment the line below to fetch the students from the API
+// getStudents();
+
+const postStudent = async () => {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(student),
+  });
+  const data = await response.json();
+  console.log(data);
+};
+// uncomment the line below to post yourself to the API
+// postStudent();
